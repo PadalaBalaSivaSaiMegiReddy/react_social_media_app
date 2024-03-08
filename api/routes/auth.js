72a659eb57cbf9306aea516c436b0b067663aee9
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-08 19:44:25 +0530

    Add user description, city, from, and relationship fields*** ***Implement login functionality*** ***Update user information*** ***Delete user functionality

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,9 +22,31 @@ router.post('/register',async(req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user);
     } catch (error) {
-        console.log(error)
+        res.status(500).json(error)
     }
 
+})
+
+//LOGIN 
+
+router.post('/login',async(req,res)=>{
+    try {
+        const user = await User.findOne({
+            email:req.body.email
+        })
+        if(!user){
+            res.status(404).json("user not found")
+        }
+        const validPassword=await bcrypt.compare(req.body.password,user.password);
+        if(!validPassword){
+            res.status(400).json("user not found")
+        }
+        res.status(200).json(user._id);
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
 })
 
 module.exports=router
