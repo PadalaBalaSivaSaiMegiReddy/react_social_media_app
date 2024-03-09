56a743828f6eadd13922b04f0304f0cbbc162041
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-09 17:32:19 +0530

    Add like/dislike functionality and timeline posts API

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,7 @@
 const Post=require("../models/Post");
 const router = require("express").Router();
+const User=require("../models/User")
+
 
 //create a post 
 
@@ -54,7 +56,65 @@ router.put("/:id", async (req, res) => {
 
 
 
-//like a post
+//like/dislike a post
+
+router.put("/:id/like",async(req,res)=>{
+  try {
+    const post = await Post.findById(req.params.id);
+      if(!post.likes.includes(req.body.userId)){
+        // currentUser.following.push(req.params.id);
+        // user.followers.push(req.body.userId);
+        await post.updateOne({$push:{
+          likes:req.body.userId
+        }});
+        res.status(200).json("The post has been liked");
+
+      }else{
+        await post.updateOne({$pull:{likes:req.body.userId}});
+        res.status(200).json("The post has been disliked");
+
+
+      }
+
+      
+
+    
+  } catch (error) {
+    res.status(500).json(error)
+  }
+
+
+})
+
+
 //get a post 
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //get timeline posts
+
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
