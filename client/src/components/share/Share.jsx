1fbe96f6f7fa01649e1d4851e37d847e86ca3db0
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-11 13:07:15 +0530

    Add Share component to Feed.jsx

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,20 @@
+import './Share.css'
+
+function Share() {
+  return (
+    <div className='share'>
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img src="../src/assets/person/1.jpeg" className='shareProfileImg' alt="" />
+                <input className='shareInput' type="text" placeholder="What's in you mind Megi?"/>
+            </div>
+            <hr className='shareHr'/>
+            <div className="shareBottom">
+
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default Share
