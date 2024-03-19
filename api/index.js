287dca19145e2f5b9465f1354b71fdbea4bc809b
BalaSivaSaiMegiReddyPadala <megireddy21@gmail.com>
2024-03-19 23:06:18 +0530

    Add multer for file upload and update package.json

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const helmet= require('helmet');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const multer  = require('multer');
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
 const postRoute = require('./routes/posts')
+
 
 
 
@@ -29,6 +31,29 @@ app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'))
+const upload = multer({ storage: storage });
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "src/assets");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+
+app.post('http://localhost:8800/api/upload', upload.single('file'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    try {
+        return res.status(200).json("File uploaded successfully");
+        
+    } catch (error) {
+        console.log(error)
+    }
+  })
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
